7ee0552155ff707b5da994203aa5126d2c2d89e0
Daniel Buergler <daniel.jbuergler@gmail.com>
2020-11-18 21:41:03 -0500

    second commit

---
+++ challenges/weekTwo/loops.js
@@ -0,0 +1,49 @@
+//for(initialExpression; condition; increment ){
+
+//}
+
+//console.log("Hello")
+
+//for(let i=0; i< 10; i++){
+    //console.log("Hello", i);
+//}
+
+// let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// for (let i=0; i<alphabet.length; i++){
+//    console.log(alphabet[i]);
+// }
+
+
+// let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// for (let i=0; i<alphabet.length; i++){
+//     if([i] %2 ==0) {
+//         console.log(alphabet[i]);
+//     } else {
+//         console.log("the index is an odd number");
+//     }
+//     //console.log(alphabet[i]);
+//  }
+
+
+ 
+//  let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//  let arr= [];
+
+// for (let i=0; i<alphabet.length; i++){
+//     if([i] %2 ==0) {
+//         arr.push(alphabet[i]);
+//     } else {
+//         console.log(`the index ${i} is an odd number`);
+//     }
+    
+//  }
+
+//  console.log(arr);
+
+
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let arr = [];
+for (let i = 0; i < alphabet.length; i++) {
+    (i % 2 === 0) ? (arr.push(alphabet[i]), console.log(arr)) : console.log(`the index ${i} is an odd number`);
+}
